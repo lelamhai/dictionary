3ca7d4b90fcbcdc273d60339cf5b1f7398452db2
lelamhai <leelamhair@gmail.com>
2022-10-17 23:54:11 +0700

    database

--- wp-content/themes/dictionary/header.php
+++ wp-content/themes/dictionary/header.php
@@ -1,6 +1,7 @@
-<?php 
-$text = "
-   
-    ";
-
-    
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Page Title</title>
+        <meta charset="UTF-8">
+    </head>
+    <body>
---
+++ wp-content/themes/dictionary/read-data.php
@@ -0,0 +1,18 @@
+<?php 
+      $args = array(  
+        'post_type' => 'dictionary',
+        'post_status' => 'publish',
+        'posts_per_page' => -1, 
+        'order' => 'ASC', 
+        'option' => true,
+        'custom_search' => 'c',
+
+    );
+    $loop = new WP_Query( $args ); 
+        
+    while ( $loop->have_posts() ) : $loop->the_post(); 
+        echo the_title(); echo "<br>";
+    endwhile;
+
+    wp_reset_postdata(); 
+?>
